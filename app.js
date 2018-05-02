2f0c640ebc9d3212916a535ab9d44d8379c0f193
Jeb Palo <jeb.palo07@gmail.com>
2018-05-02 11:52:26 -0400

    show-hide-settings

--- app.js
+++ app.js
@@ -42,7 +42,8 @@ function getFoursquareApi(latitude, longitude) {
         data: {
             ll: latitude + "," + longitude,
             categoryId: '4d4b7105d754a06376d81259',
-            limit: 50
+            limit: 50,
+            sortByDistance: 1
         },
         dataType: 'json',
         type: 'GET',
@@ -83,3 +84,31 @@ ${result.venue.categories[0].name}
 </div>
 `;
 }
+
+//function getCoffeeResults(latitude, longitude) {
+//    $("img[src='left1.gif']").on('click', function (event) {
+//        alert("do something");
+//        $('.start-section').hide();
+//        $('.thumb').hide();
+//    })
+//};
+//
+//getCoffeeResults();
+//
+//            function displayCoffeeResults(result) {
+//                return `
+//<div class="result col-3">
+//<h2 class="result-name"><a href="${result.venue.url}" target="_blank">${result.venue.name}</a></h2>
+//<span class="icon">
+//<img src="${result.venue.categories[0].icon.prefix}bg_32${result.venue.categories[0].icon.suffix}" alt="category-icon">
+//</span>
+//<span class="icon-text">
+//${result.venue.categories[0].name}
+//</span>
+//<p class="result-address">${result.venue.location.formattedAddress[0]}</p>
+//<p class="result-address">${result.venue.location.formattedAddress[1]}</p>
+//<p class="result-address">${result.venue.location.formattedAddress[2]}</p>
+//</div>
+//</div>
+//`;
+//            }
