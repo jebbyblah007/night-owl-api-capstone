2bb82f252b3bfc84c84b290ec7d689cd711a021f
Jenniber Palo <jeb.palo07@gmail.com>
2018-05-08 11:16:02 -0400

    recovering old repository

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # night-owl-api-capstone
-Night Owl is a simple app that would allow users to find cafes and restaurants that are open late at night, near them, or around a specific zip code. 
+Night Owl is a simple app that would allow users to find cafes and restaurants that are open late at night, near them, or around a specific zip code.
 
 ![Landing page top Screenshot](https://github.com/LeahBorns/commute-calculator-final-capstone-react/blob/master/wire-frame-images/landing-page-top.png)
 ![Landing page bottom Screenshot](https://github.com/LeahBorns/commute-calculator-final-capstone-react/blob/master/wire-frame-images/landing-page-bottom.png)
@@ -16,15 +16,15 @@ Why is this app useful? This simplicity of this app will immediately cater to th
 * As a user I should be able to understand the purpose of the app based off the landing page to use the app.
 * As a user, I should be able to allow or deny the computer to know my location or enter my zip code in order to search cafes and restaurants near me.
 * As a user I should be able to see the place’s details including its name, opening hours, miles away, rating, photos, and link to the place’s website.
-* As a user I should be able to return to the landing page to start a new search. 
+* As a user I should be able to return to the landing page to start a new search.
 
 ##### Screen Inventory
 * Landing page with 2 options
 * Map with icons
-* Info window to display place’s details 
+* Info window to display place’s details
 
 ##### Wireframes
-* Wireframe to understand purpose. 
+* Wireframe to understand purpose.
 ![Purpose and Sign-up](https://github.com/LeahBorns/commute-calculator-final-capstone/blob/master/wire-frame-images/landing-signup-html-wireframe.png)
 * Wireframe to show the search forms including the “near me,” “zip code,” and “late night” options.
 * Wireframes to show the map with the point of interest pins with the results under it.
@@ -36,7 +36,7 @@ You can access a working prototype of the app here: https://jebbyblah007.github.
 The app's functionality includes:
 
 * Landing page that includes three buttons – one to search places near the user’s location, one to search places near a specific zip code, and another option to search for late night restaurants and cafes.
-* An arrow button that will take the user back to the landing page. 
+* An arrow button that will take the user back to the landing page.
 
 
 ## Technical
--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 "use strict";
-
 function startHere() {
     $(document).ready(function () {
         $('.start-section').show();
@@ -7,7 +6,7 @@ function startHere() {
     })
 }
 startHere();
-const FOURSQUARE_SEARCH_URL = "https://api.foursquare.com/v2/venues/explore?&client_id=0EFO1LZES0FQBZIXAEGMZNWXHHDAIZZCJ10LWFCHKIP4LWCV&client_secret=XX4GIMFENPBMUTOMGCN1CI0JEG2IFGYSBTLUV3ASHBHQZNMV&v=20180418"
+const FOURSQUARE_SEARCH_URL = "https://api.foursquare.com/v2/venues/search?&client_id=0EFO1LZES0FQBZIXAEGMZNWXHHDAIZZCJ10LWFCHKIP4LWCV&client_secret=XX4GIMFENPBMUTOMGCN1CI0JEG2IFGYSBTLUV3ASHBHQZNMV&v=20180418"
 //get current location
 function geoFindMe() {
     var output = document.getElementById("out");
@@ -15,25 +14,23 @@ function geoFindMe() {
         output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
         getFoursquareApi("", "", "");
     }
-
     function success(position) {
         var latitude = position.coords.latitude;
         var longitude = position.coords.longitude;
-
         //        output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
         $('.container').show();
+        /*$('#trial:has(img[alt="food.jpg"])').click(function() {
+  alert( "SUCCESS" );
+});*/
         $('.start-section').hide();
         $('.thumb').hide();
         getFoursquareApi(latitude, longitude, "");
     }
-
     function error() {
         output.innerHTML = "Unable to retrieve your location";
         getFoursquareApi("", "", "");
     }
-
     output.innerHTML = "<p>Locating…</p>";
-
     navigator.geolocation.getCurrentPosition(success, error);
 }
 //get foursquare info
@@ -43,72 +40,72 @@ function getFoursquareApi(latitude, longitude) {
             ll: latitude + "," + longitude,
             categoryId: '4d4b7105d754a06376d81259',
             limit: 50,
-            sortByDistance: 1
+            radius: 8047,
         },
         dataType: 'json',
         type: 'GET',
         success: function (data) {
             console.log(data);
-            if (data.response.totalResults != 0) {
-                let results = data.response.groups[0].items.map(function (item, index) {
-                    console.log(item);
-                    return displayResults(item);
-                });
-                $('#foursquare-results').html(results);
-                scrollPageTo('#foursquare-results', 15);
-            } else {
-                $('#foursquare-results').html("<div class='result'><p>No Results Found</p></div>");
-            }
-
+            let results = data.response.venues.map(function (item, index) {
+                console.log(item);
+                //console.log(item.id);
+//                if (item.categories[0].id === '4bf58dd8d48988d1e0931735' || item.categories[0].id === '4bf58dd8d48988d16d941735' || item.categories[0].id === '4bf58dd8d48988d114951735') {
+//                    console.log(item); //coffee shops, cafes, or bookstores
+//
+//                } else {
+//
+//                }
+                return displayResults(item);
+            });
+            $('#foursquare-results').html(results);
+            //scrollPageTo('#foursquare-results', 15);
         },
         error: function () {
-            $('#foursquare-results').html("<div class='result'><p>Whoopsie! Let's try that again.</p></div>");
+            $('#foursquare-results').html("<div class='result'><p>Error. No data found.</p></div>");
         }
     });
 }
-
 function displayResults(result) {
     return `
-<div class="result col-3">
-<h2 class="result-name"><a href="${result.venue.url}" target="_blank">${result.venue.name}</a></h2>
+<div class="result ${result.categories[0].name} col-3">
+<h2 class="result-name"><a href="${result.url}" target="_blank">${result.name}</a></h2>
 <span class="icon">
-<img src="${result.venue.categories[0].icon.prefix}bg_32${result.venue.categories[0].icon.suffix}" alt="category-icon">
+<img src="${result.categories[0].icon.prefix}bg_32${result.categories[0].icon.suffix}" alt="category-icon">
 </span>
 <span class="icon-text">
-${result.venue.categories[0].name}
+${result.categories[0].name}
 </span>
-<p class="result-address">${result.venue.location.formattedAddress[0]}</p>
-<p class="result-address">${result.venue.location.formattedAddress[1]}</p>
-<p class="result-address">${result.venue.location.formattedAddress[2]}</p>
+<p class="result-address">${result.location.formattedAddress[0]}</p>
+<p class="result-address">${result.location.formattedAddress[1]}</p>
+<p class="result-address">${result.location.formattedAddress[2]}</p>
 </div>
 </div>
 `;
 }
+$(document).on('click', '#showFood', function (event) {
+    event.preventDefault();
+    $('.result').hide();
+    $('.Restaurant').show();
+    $('.Pizza').show();
+});
+$(document).on('click', '#showBars', function (event) {
+    event.preventDefault();
+    $('.result').hide();
+    $('.Bar').show();
+    $('.Beer').show();
+    $('.Brewery').show();
+});
+$(document).on('click', '#showCoffee', function (event) {
+    event.preventDefault();
+    $('.result').hide();
+    $('.Coffee').show();
+    $('.Cafe').show();
+    $('.Bookstore').show();
+});
+$(document).on('click', '#goBack', function (event) {
+    event.preventDefault();
+    $('.start-section').show();
+    $('.result').hide();
+    $('.container').hide();
+});
 
-//function getCoffeeResults(latitude, longitude) {
-//    $("img[src='left1.gif']").on('click', function (event) {
-//        alert("do something");
-//        $('.start-section').hide();
-//        $('.thumb').hide();
-//    })
-//};
-//
-//getCoffeeResults();
-//
-//            function displayCoffeeResults(result) {
-//                return `
-//<div class="result col-3">
-//<h2 class="result-name"><a href="${result.venue.url}" target="_blank">${result.venue.name}</a></h2>
-//<span class="icon">
-//<img src="${result.venue.categories[0].icon.prefix}bg_32${result.venue.categories[0].icon.suffix}" alt="category-icon">
-//</span>
-//<span class="icon-text">
-//${result.venue.categories[0].name}
-//</span>
-//<p class="result-address">${result.venue.location.formattedAddress[0]}</p>
-//<p class="result-address">${result.venue.location.formattedAddress[1]}</p>
-//<p class="result-address">${result.venue.location.formattedAddress[2]}</p>
-//</div>
-//</div>
-//`;
-//            }
